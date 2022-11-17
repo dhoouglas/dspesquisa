c0ce863e3fdc4e474cf63c3bfc3122749946c9ed
Douglas Araujo <dhoouglas@hotmail.com>
2022-11-17 10:59:17 -0300

    Chart page added along with backend

--- frontend/src/Routs.tsx
+++ frontend/src/Routs.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route  } from 'react-router-dom';
 import Header from './components/Header';
+import Charts from './pages/Charts';
 import Home from './pages/Home';
 import Records from './pages/Records';
 
@@ -13,6 +14,10 @@ function Routs() {
 
             <Routes>
                 <Route path="/records" element={<Records/>} />
+            </Routes>
+
+            <Routes>
+                <Route path="/charts" element={<Charts/>} />
             </Routes>
         </BrowserRouter>
     );
